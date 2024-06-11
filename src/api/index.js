f05c320f3784175fb5d116889fcf147930bc1163
varghesesabu2889 <varghesesabu16@gmail.com>
2024-06-11 20:59:50 +0530

    new commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,15 @@
-import { collection, doc, onSnapshot, orderBy, query, setDoc } from "firebase/firestore";
+// Firebase API functions
+import { arrayRemove, collection, doc, onSnapshot, orderBy, query, setDoc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../config/firebase.config";
+import { arrayUnion } from "firebase/firestore/lite";
+import { toast } from "react-toastify";
 
 export const getUserDetail = () => {
   return new Promise((resolve, reject) => {
     const unsubscribe = auth.onAuthStateChanged((userCred) => {
       if (userCred) {
         const userData = userCred.providerData[0];
-        
+
         const unsubscribeSnapshot = onSnapshot(
           doc(db, "users", userData?.uid),
           (_doc) => {
@@ -32,16 +35,43 @@ export const getUserDetail = () => {
   });
 };
 
-export const getTemplates =()=>{
-  return new Promise((resolve,reject)=>{
+export const getTemplates = () => {
+  return new Promise((resolve, reject) => {
     const templateQuery = query(
-      collection(db,"templates"),
-      orderBy("timestamp","asc")
-    )
-    const unsubscribe =onSnapshot(templateQuery,(querySnap)=>{
-      const templates = querySnap.docs.map((doc)=>doc.data);
+      collection(db, "templates"),
+      orderBy("timestamp", "asc")
+    );
+    const unsubscribe = onSnapshot(templateQuery, (querySnap) => {
+      const templates = querySnap.docs.map((doc) => doc.data());
       resolve(templates);
-    })
-    return ()=>unsubscribe;
-  })
-}
+    });
+    return () => unsubscribe;
+  });
+};
+
+
+
+// Function to add or remove an item from user's collection
+export const toggleCollectionItem = async (user, data) => {
+  const docRef = doc(db, 'users', user?.uid);
+  let message = '';
+
+  try {
+    if (user?.collections?.includes(data?._id)) {
+      await updateDoc(docRef, {
+        collections: arrayRemove(data?._id),
+      });
+      message = 'Removed from Collection';
+    } else {
+      await updateDoc(docRef, {
+        collections: arrayUnion(data?._id),
+      });
+      message = 'Added to Collection';
+    }
+
+    toast.success(message);
+    // Perform any additional actions after updating collection
+  } catch (err) {
+    toast.error(`Error: ${err.message}`);
+  }
+};
--- src/components/TemplateDesignPin.jsx
+++ src/components/TemplateDesignPin.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { scaleInOut } from '../animation';
-
+import React, { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { FadeInOutWithOpacity, scaleInOut } from '../animation';
+import {  BiFolderPlus, BiHeart } from 'react-icons/bi'
 const TemplateDesignPin = ({ data, index }) => {
+
+const addToCollection = async() =>{
+
+}
+const addToFavorites = async()=>{
+
+}
+
+
   return (
     <motion.div key={data?._id} {...scaleInOut(index)}>
       <div className="w-full h-[240px] 2xl:h-[450px] rounded-md bg-gray-200 overflow-hidden relative">
         <img src={data.imageURL} className="w-full h-full object-cover" alt="" />
 
+<AnimatePresence>
+  <motion.div
+    {...FadeInOutWithOpacity}
+    className='absolute inset-0 bg-[rgba(0,0,0,0.4)] flex flex-col items-center justify-start px-4 py-3 z-50 cursor-pointer'
+    >
+<div className='flex flex-col items-end justify-start w-full gap-8'>
+  <InnerBoxCard Label={"Add to Collection"} Icon={BiFolderPlus} OnHandle={addToCollection}/>
+  <InnerBoxCard Label={"Add to Favorites"} Icon={BiHeart} OnHandle={addToFavorites}/>
 
+  </div>    
+
+  </motion.div>
+</AnimatePresence>
 
 
 
@@ -19,5 +40,35 @@ const TemplateDesignPin = ({ data, index }) => {
     </motion.div>
   );
 };
+
+
+const InnerBoxCard = ({Label,Icon,OnHandle}) =>{
+  const [isHovered, setIsHovered] = useState(false)
+return(
+<div onClick={OnHandle} className='w-10 h-10 rounded-md bg-gray-300 flex items-center justify-center 
+      hover:shadow-md relative'
+      onMouseEnter={()=>setIsHovered(true)}
+      onMouseLeave={()=>setIsHovered(false)}>
+  <Icon className='text-txtPrimary text-base'/>
+  <AnimatePresence>
+    {isHovered && (
+      <motion.div  
+      initial={{opacity:0, scale:0.6, x:50}}
+      animate={{opacity:1, scale:1, x:0}}
+      exit={{opacity:0, scale:0.6, x:50}}
+      className='px-3 py-2 rounded-md bg-gray-200 absolute -left-40 after:w-2 
+                after:h-2 after:bg-amber-600 after:absolute after:-right-1 after:top-[14px] after:rotate-45'>
+        <p className='text-sm text-txtPrimary whitespace-nowrap'>{Label}</p>
+      </motion.div>
+    )}
+  </AnimatePresence>
+
+
+
+
+
+</div>
+)
+}
 
 export default TemplateDesignPin;
